ebebbc643c337a3a517aed4742d82b5485a0869b
sorachung <sorachung.src@gmail.com>
2022-02-28 11:16:30 -0600

    added guess number and ends loop when guessing correctly

--- Program.cs
+++ Program.cs
@@ -2,19 +2,23 @@
 
 int secretNumber = 42;
 int guessesLeft = 4;
+int guessNumber = 1;
 
-while (guessesLeft > 0)
+while (guessNumber <= guessesLeft)
 {
+    Console.WriteLine($"Your guess - {guessNumber}");
     Console.Write("Please guess the Secret Number: ");
     string guess = Console.ReadLine();
 
     if (int.Parse(guess) == secretNumber)
     {
         Console.WriteLine("Congratulations! You guessed correctly!");
+        break;
     }
     else
     {
         Console.WriteLine("You are WRONG!");
     }
-    guessesLeft--;
+    guessNumber++;
+
 }
